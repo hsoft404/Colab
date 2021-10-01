cec8382f7e8d617a85316d345bb546e9d6e58970
Hsoft404 <77140115+ProWebDev21@users.noreply.github.com>
2021-10-01 21:42:12 +0530

    Add files via upload

---
+++ appindex.js
@@ -0,0 +1,9 @@
+const CREDENTIALS = {
+    appId: 5238,
+    authKey: "FjMV9tx57tzCS8W",
+    authSecret: "nNp3nCUvZBCW2u6",
+  };
+  const CONFIG = {
+  debug: { mode: 1 },
+};
+ConnectyCube.init(CREDENTIALS, CONFIG);
